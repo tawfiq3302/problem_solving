54fcdb29b2cc942c4bed4e8eb6866b329e51d2cc
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-29 17:36:22 +0300

    q1

--- level1/question1.cpp
+++ level1/question1.cpp
@@ -1,1 +1,17 @@
+// Level: 1
+// Question: Print personal information (name, age, city, country)
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "**************************\n";
+    cout << "Name: Tawfiq Abu Rumman\n";
+    cout << "Age: 24\n";
+    cout << "City: Salt\n";
+    cout << "Country: Jordan\n";
+    cout << "**************************\n";
+
+    return 0;
+}
