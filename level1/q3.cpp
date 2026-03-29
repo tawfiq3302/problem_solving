0685f818e3656e570a14f336b726930e8fc7dd9a
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-29 17:59:57 +0300

    q3.cpp

---
+++ level1/q3.cpp
@@ -0,0 +1,15 @@
+// Level: 1
+// Question: Print motivational message
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I Love Programming\n\n";
+    cout << "I Promise to be the best developer ever!\n\n";
+    cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
+    cout << "Best Regards\nTawfiq Abu Rumman\n";
+
+    return 0;
+}
