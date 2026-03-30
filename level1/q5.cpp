e6a831a2055ca58fcb8f25365a735d4df5a4ce5d
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-30 18:42:24 +0300

    q5.cpp

---
+++ level1/q5.cpp
@@ -0,0 +1,15 @@
+// Level: 1
+// Question: Calculate age after 5 years
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 25;
+
+    cout << "After 5 years you will be "
+         << age + 5 << " years old\n";
+
+    return 0;
+}
