6ee2050baa3717ac0a06c4bfb6b40226028e011c
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-30 18:47:46 +0300

    q6.cpp

---
+++ level1/q6.cpp
@@ -0,0 +1,18 @@
+// Level: 1
+// Question: Calculate sum of 3 numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1 = 20, number2 = 30, number3 = 10;
+
+    cout << number1 << "+\n";
+    cout << number2 << "+\n";
+    cout << number3 << "\n\n";
+    cout << "------------------\n\n";
+    cout << "Total = " << number1 + number2 + number3 << endl;
+
+    return 0;
+}
