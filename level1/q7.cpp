2724e7dc84ad09d8e786aef7a7edde1e2ac6bb7b
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-30 18:54:21 +0300

    q7.cpp

---
+++ level1/q7.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+      
+    string Name = "Tawfiq Abu Rumman";
+    string Age = "24 years";
+    string City = "AL-SALT";
+    string Country = "JORDAN";
+    float Monthly_salary = 500;
+    char Gender = 'M';
+    bool Married = false;
+
+  
+    cout << "*********************************\n";
+    cout << "Name:" << Name << endl;
+    cout << "Age:" << Age << endl;
+    cout << "City:" << City << endl;
+    cout << "Country:" << Country << endl;
+    cout << "Monthly_salary:" << Monthly_salary << endl;
+    cout << "yearly_salary:" << Monthly_salary * 12 << endl;
+    cout << "Gender:" << Gender << endl;
+    cout << "married:" << Married << endl;
+    cout << "*********************************\n";
+
+
+    return 0;
+}
+
