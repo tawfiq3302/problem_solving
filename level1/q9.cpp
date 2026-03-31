9f155fb172ec04220d57d0f362dd587f76820286
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-31 17:18:36 +0300

    q9.cpp

---
+++ level1/q9.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+  
+    int Age;
+        cout << "please enter your age: \n";
+        cin >> Age;
+        cout << "after 5 years you will be " << Age + 5<<" years old\n";
+
+    return 0;
+}
