07d7eb8a2092becafab3d44d5f22df1dad3148c4
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-03-31 21:46:48 +0300

    q10.cpp

---
+++ level1/q10.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    int number1, number2, number3;
+    cout << "please enter number 1\n";
+    cin >> number1;
+    cout << "please enter number 2\n";
+    cin >> number2;
+    cout << "please enter number 3\n";
+    cin >> number3;
+
+
+
+    cout<<"\n" << number1 << "+\n";
+    cout << number2 << "+\n";
+    cout << number3 << "\n\n";
+    cout << "------------------\n\n";
+    cout << "Total = " << number1 + number2 + number3 << endl;
+
+    return 0;
+}
+
