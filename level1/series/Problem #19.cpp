0011ee55d491554b02c991d22221a057e0ac363d
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-06 09:48:06 +0300

    Problem #19.cpp

---
+++ level1/series/Problem #19.cpp
@@ -0,0 +1,17 @@
+// question=> Write a program to calculate circle area through diameter, then print it on the screen.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+   
+    float circle_AreaBy_Diameter, diameter;
+    const float pi = 3.14;
+    cout << "please enter the diameter of the circle:\n";
+    cin >> diameter;
+    circle_AreaBy_Diameter = pi * diameter * diameter / 4;
+    cout << "the area of circle by daimeter is " << circle_AreaBy_Diameter;
+    return 0;
+}
