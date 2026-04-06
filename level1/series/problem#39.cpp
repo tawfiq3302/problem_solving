7dbd858b45d0590518789d0e9feddb6ef7baac04
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-06 13:03:02 +0300

    problem#39.cpp

---
+++ level1/series/problem#39.cpp
@@ -0,0 +1,24 @@
+//question=>Write a program to read a TotalBill and CashPaid and calculate the remainder to be paid back.
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+ 
+    cout << "this program calculate remaining balance of the bill\n";
+    float totalBill, cashpaid;
+    cout << "please enter the totalBill\n";
+    cin >> totalBill;
+    cout << "please enter the cashpaid\n";
+    cin >> cashpaid;
+
+    cout << "the remaining amount paid is " << cashpaid - totalBill;
+
+    
+
+    return 0;
+}
+
