84609202ca568b975833fda0f5990d98d539b53b
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-09 18:25:37 +0300

    Problem #20.cpp

--- level1/series/Problem #20.cpp
+++ level1/series/Problem #20.cpp
@@ -3,16 +3,17 @@
 
 #include <iostream>
 #include <string>
+#include<cmath>
 using namespace std;
 int main()
 {
-   
+
     float circle_Area_Inscribed_INsquare, area_ofsquare;
     const float pi = 3.14;
     cout << "please enter the area of square \n";
     cin >> area_ofsquare;
-    circle_Area_Inscribed_INsquare = pi * area_ofsquare * area_ofsquare / 4;
+    circle_Area_Inscribed_INsquare =ceil( pi * pow(area_ofsquare,2) / 4);
     cout << "the area of circle inscribed the square is " << circle_Area_Inscribed_INsquare;
 
-    return 0;
+        return 0;
 }
