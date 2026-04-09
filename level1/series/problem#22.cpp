d101ae76f96642cd7982312aa341750064c5426c
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-09 22:37:52 +0300

    problem#22.cpp

--- level1/series/problem#22.cpp
+++ level1/series/problem#22.cpp
@@ -4,10 +4,12 @@
 
 #include <iostream>
 #include <string>
+#include<cmath>
 using namespace std;
 int main()
 {
-   
+
+
     float AreaOf_Circle_inside_AnIsosceles_triangle, first_side, seconde_side;
     const float pi = 3.14;
     cout << "this program to calculate the area of circle inside an isosceles triangle \n";
@@ -15,8 +17,9 @@ int main()
     cin >> first_side;
     cout << "please enter the seconde side \n";
     cin >> seconde_side;
-    AreaOf_Circle_inside_AnIsosceles_triangle = pi * seconde_side * seconde_side / 4 * ((2 * first_side - seconde_side) / (2 * first_side + seconde_side));
-    cout << "this is the area of circle " << AreaOf_Circle_inside_AnIsosceles_triangle;
+    AreaOf_Circle_inside_AnIsosceles_triangle = pi * pow(seconde_side,2)  / 4 * ((2 * first_side - seconde_side) / (2 * first_side + seconde_side));   
+    cout << "this is the area of circle " << floor(AreaOf_Circle_inside_AnIsosceles_triangle);
 
-    return 0;
+
+        return 0;
 }
