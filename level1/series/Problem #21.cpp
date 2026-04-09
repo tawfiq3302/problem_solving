aa5c510b7f537f7621dd1a8dec933b4d8192c785
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-09 22:28:26 +0300

    Problem #21.cpp

--- level1/series/Problem #21.cpp
+++ level1/series/Problem #21.cpp
@@ -3,16 +3,19 @@
 
 #include <iostream>
 #include <string>
+#include<cmath>
 using namespace std;
 int main()
 {
-   
-    float circle_Area_byCircumference,circumference ;
+
+
+    float circle_Area_byCircumference, circumference;
     const float pi = 3.14;
     cout << "please enter circumference to calculate the area circle:\n";
     cin >> circumference;
-    circle_Area_byCircumference = (circumference * circumference) /( pi * 4);
-    cout << "this the area of circle " << circle_Area_byCircumference;
+    circle_Area_byCircumference = pow(circumference,2)  / (pi * 4);
+    cout << "this the area of circle " <<floor( circle_Area_byCircumference);
 
-    return 0;
+
+        return 0;
 }
