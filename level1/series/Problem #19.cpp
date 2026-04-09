dcaa3708d4463b3944a15fa59d56689aeba21f48
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-09 18:21:27 +0300

    Update Problem #19.cpp

--- level1/series/Problem #19.cpp
+++ level1/series/Problem #19.cpp
@@ -3,15 +3,16 @@
 
 #include <iostream>
 #include <string>
+#include<cmath>
 using namespace std;
 int main()
 {
-   
+
     float circle_AreaBy_Diameter, diameter;
     const float pi = 3.14;
     cout << "please enter the diameter of the circle:\n";
     cin >> diameter;
-    circle_AreaBy_Diameter = pi * diameter * diameter / 4;
+    circle_AreaBy_Diameter = ceil(pi * pow(diameter,2) / 4);
     cout << "the area of circle by daimeter is " << circle_AreaBy_Diameter;
-    return 0;
+        return 0;
 }
