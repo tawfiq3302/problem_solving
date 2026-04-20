a59d925d83683f6fe63a5136ac7960b6a9f3f339
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-20 10:35:13 +0300

    problem#1.cpp

---
+++ level1/series/problem#1.cpp
@@ -0,0 +1,24 @@
+// question=> write a program to print your name on screen 
+
+
+#include <iostream>
+#include <string>
+#include<cmath>
+using namespace std;
+
+
+void PrintMyName(string name)
+{
+    cout << name;
+
+}
+
+
+int main()
+{
+   
+    PrintMyName("Tawfiq Abu Rumman\n");
+
+
+    return 0;
+} 
