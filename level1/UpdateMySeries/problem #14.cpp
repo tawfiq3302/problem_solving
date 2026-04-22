ea4f17c4e64493c0ab11040881d5ffd79ee9949c
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-22 10:42:44 +0300

    problem #14.cpp

--- level1/UpdateMySeries/problem #14.cpp
+++ level1/UpdateMySeries/problem #14.cpp
@@ -1,1 +1,43 @@
+// Question: Write a program to swap two numbers using a temporary variable and print the values before and after swapping.
 
+
+#include <iostream>
+#include <string>
+#include<cmath>
+using namespace std;
+
+
+void PrintNumbers(short number1, short number2)
+{
+    cout << number1 << endl;
+    cout << number2 << endl<<endl;
+}
+
+void PrintSwappedNumbers(short num1,short num2)
+{
+    cout << num1 << endl;
+    cout << num2 << endl;
+
+}
+
+
+
+int main()
+{
+    short number1, number2,temp;
+    cout << "please enter number 1\n";
+    cin >> number1;
+    cout << "please enter number 2\n";
+    cin >> number2;
+    cout << endl;
+
+    PrintNumbers(number1, number2);
+
+    temp = number1;
+    number1 = number2;
+    number2 = temp;
+    PrintSwappedNumbers(number1, number2);
+
+    return 0;
+
+}
