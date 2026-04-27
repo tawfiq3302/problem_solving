ed60ed118a81de380fc7998ba551c1fb111b0620
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-04-27 15:23:22 +0300

    q7.cpp

--- level2/q7.cpp
+++ level2/q7.cpp
@@ -1,33 +1,25 @@
-// Question: Write a function to calculate the average of three numbers using an array
-
+// Question: Write a program to store three numbers in an array and calculate their average.
 
 
 #include <iostream>
 #include <string>
-#include<cmath>
 using namespace std;
 
-float CalculateAverage(float num1, float num2,float num3)
-{
-    
-    float num[3]={num1,num2,num3};
-    return (num[0] + num[1] + num[2]) / 3;
-}
 
 
 int main()
 {
-    float num1, num2, num3;
+    float num[3];
 
     cout << "please enter num1\n";
-    cin >> num1;
+    cin >> num[0];
     cout << "please enter num2\n";
-    cin >> num2;
+    cin >> num[1];
     cout << "please enter num3\n";
-    cin >> num3;
+    cin >> num[2];
 
-    cout<<"average = " << CalculateAverage(num1, num2, num3);
-
+    cout << "average = " << (num[0] + num[1] + num[2]) / 3;
     return 0;
 
 }
+
