a95569b9c36e40fbdecf4038a1e7222e81760dd6
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-05-06 11:42:03 +0300

    problem#49.cpp

---
+++ series/problem#49.cpp
@@ -0,0 +1,36 @@
+//Write a program to read the ATM PIN code from the user, then check if PIN Code = 1234, then show the balance to user,
+//otherwise print "Wong PIN" and ask the user to enter the PIN again.  
+//Assume User Balance is 7500.
+
+
+#include <iostream>
+#include <string>
+#include<cmath>
+using namespace std;
+
+void ReadPINCode(string &PIN_Code)
+{
+    cout << "Please Enter Your PIN Code\n";
+    cin >> PIN_Code;
+}
+
+void CheckPINAndPrintYourBalance(string PIN)
+{
+    if (PIN == "1234")
+    {
+        cout << "your Balance is 7500\n" << endl;
+    }
+    else
+    {
+        cout << "wrong PIN \n";
+    }
+}
+
+int main()
+{
+    string PIN;
+    ReadPINCode(PIN);
+    CheckPINAndPrintYourBalance(PIN);
+        return 0;
+
+}
