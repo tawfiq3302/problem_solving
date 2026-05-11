0d7d2ade6e96d855d61b6d45f0258a2efe8e31c4
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-05-11 14:55:13 +0300

    problem#49.cpp

--- series/problem#49.cpp
+++ series/problem#49.cpp
@@ -1,7 +1,7 @@
 //Write a program to read the ATM PIN code from the user, then check if PIN Code = 1234, then show the balance to user,
 //otherwise print "Wong PIN" and ask the user to enter the PIN again.  
 //Assume User Balance is 7500.
-
+ 
 
 #include <iostream>
 #include <string>
