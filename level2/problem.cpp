db2f67f544af161ffbb7d37e0900bf46bc2029f3
tawfiq3302 <tawfiqaburumman02@gmail.com>
2026-05-13 14:18:03 +0300

    problem.cpp

---
+++ level2/problem.cpp
@@ -0,0 +1,21 @@
+// Question => write program to print number from 1 to N
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    short N;
+    cout << "Please enter the repeat number\n";
+    cin >> N;
+    cout << endl;
+    for (short i = 1; i <=N ; i++)
+    {
+        cout << i << endl;;
+    }
+
+    return 0;
+
+}
